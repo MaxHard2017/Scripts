2e2d42398b1380e89c84ef497b9a250e248b4511
Max IV <maxhard001@gmail.com>
2023-03-29 17:58:00 +0300

    initial commit

---
+++ Bash/checkrepo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "git repo chesk"
+echo "repo: ~/projects/Javarush/JavaCollections/src"
+echo "checking git status..."
+cd ~/projects/Javarush/JavaCollections/src
+git fetch
+git status
+
+
+echo "repo: ~/projects/Javarush/JavaCore/src"
+echo "checking git status..."
+cd ~/projects/Javarush/JavaCore/src
+git fetch
+git status
+
+echo "repo: ~/projects/Javarush/JavaSyntax/src"
+echo "checking git status..."
+cd ~/projects/Javarush/JavaSyntax/src
+git fetch
+git status
+
+echo "repo: ~/Documents/Notes"
+echo "checking git status..."
+cd ~/Documents/Notes
+git fetch
+git status
+
---
+++ Win/b.bat
@@ -0,0 +1,3 @@
+cd C:\Users\BFG\projects\javarush\\JavaCollections\src
+start C:\Users\BFG\Coding\PortableGit\git-bash.exe 
+~/Scripts/Bash/checkrepo.sh
