f1d03e526828a1a50dc82dc806d8213d0250a913
Max IV <maxhard001@gmail.com>
2023-06-19 20:20:33 +0300

    c.bat corrected - launch c.sh in git-bash

--- Win/c.bat
+++ Win/c.bat
@@ -1,1 +1,2 @@
-start "" C:\Users\BFG\Coding\PortableGit\git-bash.exe -c "~/Scripts/Bash/checkallrepos.sh -f ~/Scripts/Bash/gitrepos.txt; read"
+rem start "" C:\Users\BFG\Coding\PortableGit\git-bash.exe -c "~/Scripts/Bash/c.sh -f ~/Projrcts/git_repos_list.txt; read"
+start "" C:\Users\BFG\Coding\PortableGit\git-bash.exe -c "~/Scripts/Bash/c.sh; read"
