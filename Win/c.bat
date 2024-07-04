ffed40df851a143dc2e065ec25752b462f8c5096
Max IV <99695664+MaxHard2017@users.noreply.github.com>
2024-07-04 14:30:36 +0300

    небольште правки в скрипте поиска обновлений и его запука

--- Win/c.bat
+++ Win/c.bat
@@ -1,2 +1,1 @@
-rem start "" C:\Users\BFG\Coding\PortableGit\git-bash.exe -c "~/Scripts/Bash/c.sh -f ~/Projrcts/git_repos_list.txt; read"
 start "" C:\Users\BFG\Coding\PortableGit\git-bash.exe -c "~/Scripts/Bash/c.sh; read"
